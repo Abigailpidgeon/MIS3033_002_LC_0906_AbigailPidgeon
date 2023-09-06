7bdc9313b04567577ae98395601a00b94286fee3
Abby Pidgeon <Abigail.L.Pidgeon-1@ou.edu>
2023-09-05 22:42:06 -0500

    person 1 created

---
+++ MIS3033_002_LC_0906_AbigailPidgeon/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace a
+{
+    public class Person
+    {
+        public string name {  get; set; }
+        public DateTime dob { get; set; }
+        public double weight { get; set; }
+
+        public Person() { }//empty constructor function
+
+        public Person(string name, DateTime dob, double weight)
+        {
+            this.name = name;
+            this.dob = dob;
+            this.weight = weight;
+        }
+
+    }
+}
--- MIS3033_002_LC_0906_AbigailPidgeon/Program.cs
+++ MIS3033_002_LC_0906_AbigailPidgeon/Program.cs
@@ -2,5 +2,15 @@
 // September 6, 2023
 // Abigail Pidgeon, 113515288
 
+using a;
+
 Console.WriteLine("Interface");//class interface
 
+Person p1;
+
+p1 = new Person();
+p1.name = "Abigail Pidgeon";
+p1.dob = new DateTime(2002, 8, 27);
+p1.weight = 150;// in lbs
+// this is on branch 1
+Console.WriteLine(p1.dob.ToString("MMM dd, yyyy"));
