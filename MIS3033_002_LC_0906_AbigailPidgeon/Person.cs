b9604f490df2e1ecf22786fdb0fb52e189cae911
Abby Pidgeon <Abigail.L.Pidgeon-1@ou.edu>
2023-09-06 11:41:52 -0500

    finished inclass lecture codes

--- MIS3033_002_LC_0906_AbigailPidgeon/Person.cs
+++ MIS3033_002_LC_0906_AbigailPidgeon/Person.cs
@@ -1,19 +1,28 @@
 ﻿namespace a
 {
-    public class Person
+    public class Person//: Object 
     {
-        public string name {  get; set; }
+        public double grade { get; set; } // C# syntax//you need to know 
+        public string name {  get; set; }// called member variable/field
         public DateTime dob { get; set; }
         public double weight { get; set; }
+        public virtual string CoffeeMaker()//virtual,replacable 
+        {
+            return "This is the coffee from person";
+        }
+
+
 
         public Person() { }//empty constructor function
 
+        // local variable 
         public Person(string name, DateTime dob, double weight)
         {
-            this.name = name;
+            Console.WriteLine("name");//local has priotity 
+            this.name = name;//this means self 
             this.dob = dob;
             this.weight = weight;
         }
-
+        // two functions have the same name but different parameters. called a function overload 
     }
 }
--- MIS3033_002_LC_0906_AbigailPidgeon/Program.cs
+++ MIS3033_002_LC_0906_AbigailPidgeon/Program.cs
@@ -3,14 +3,60 @@
 // Abigail Pidgeon, 113515288
 
 using a;
+using fi;
 
 Console.WriteLine("Interface");//class interface
 
-Person p1;
+a.Person p1;//person is like the blueprint,complex
+a.Person p2 = new a.Person("Abby",new DateTime(2002,8,25),140);//constructor function and function overload 
 
-p1 = new Person();
+p1 = new a.Person();// function call, constructor function 
+p1.grade = 96.6;//simple
+Console.WriteLine(p1.grade);//read the value 
 p1.name = "Abigail Pidgeon";
 p1.dob = new DateTime(2002, 8, 27);
 p1.weight = 150;// in lbs
 // this is on branch 1
 Console.WriteLine(p1.dob.ToString("MMM dd, yyyy"));
+
+
+Student stu1;//studnet,complex
+stu1 = new Student();//
+stu1.dob = new DateTime(2002,8,26);
+stu1.id = "113515288";
+stu1.grade = 97.8;
+stu1.name = "Abby P";
+
+Console.WriteLine(stu1.CoffeeMaker());
+Console.WriteLine(stu1);
+Console.WriteLine(stu1.ToString());
+
+// simple int double bool char
+// complex string student
+int age2;//int
+string str2;//string
+// var
+var age3 = 19;//var here is int determined by the right side 
+//var ag4;
+var stu3 = new Student ();// student data type 
+var stu4 = new Person();// person data type 
+
+//namespace
+//folder concept
+namespace fi //folder concept,
+{
+    public class Student: Person // none, student, complex
+    {
+        public string id { get;set;}
+        public Student() { }
+
+        public string ToString()
+        {
+            return "this is a student";
+        }
+        public override string CoffeeMaker()//
+        {
+            return "this is the coffe from the student";
+        }
+    }
+}
